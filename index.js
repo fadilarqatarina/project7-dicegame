730d5e65cf67d131da67eb506a34cb2224338a70
Fadila Rizky Qatarina <140900868+fadilarqatarina@users.noreply.github.com>
2023-08-17 17:07:44 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+var randomNumber = Math.floor(Math.random()*6)+1;
+
+var randomDiceImage = "./images/dice" + randomNumber + ".png";
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomDiceImage);
+
+
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+
+var randomDiceImage2 = "./images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src",randomDiceImage2);
+
+
+if(randomNumber>randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 Wins!";
+} else if(randomNumber2 > randomNumber) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!";
+} else {
+    document.querySelector("h1").innerHTML = "Draw";
+}
